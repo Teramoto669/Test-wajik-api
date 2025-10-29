28398500b7755b4e36ec09bc10cea8c50ab42a92
Teramoto669 <rezhashahidzindarb@gmail.com>
2025-10-29 22:45:18 +0700

    Update vercel test

--- src/services/dataFetcher.ts
+++ src/services/dataFetcher.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosResponse, type AxiosRequestConfig } from "axios";
 
 const userAgent =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
 
 export async function wajikFetch(
   url: string,
